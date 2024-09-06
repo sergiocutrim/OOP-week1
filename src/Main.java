34b7184e5d38b4ec9ab7978f074c782f6046eaf7
sergiocutrim <97315931+sergiocutrim@users.noreply.github.com>
2024-09-06 10:02:25 -0400

    third

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("teste do github para ver se esta fazendo o push de cada commit");
+        System.out.println("teste 2");
 
     }
 }
